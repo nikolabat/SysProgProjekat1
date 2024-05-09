9df98d3a7b51f525d9117792dec86fee21db1909
unknown <nikolavasic@elfak.rs>
2024-05-09 23:00:32 +0200

    gotovo

---
+++ SysProgSpotifyApp/AlbumSearch.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysProgSpotifyApp
+{
+    internal class AlbumSearch
+    {
+        private static readonly string _apiKey = "";
+        private static readonly string _baseUrl = "https://api.spotify.com/v1/search";
+        public static List<AlbumInfo> FetchAlbumInfo(string query)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                var response = client.GetAsync($"?q=Taylor&type=album&limit=1&access_token=BQBsNWmzkn4qZAyWWm7EVSvdLv8FhlhNF2uH6eF0R6ZGaGXHqvvk6IWpKjUB8if7r30hY9_A-JcEy2nMudi9csRlu0yzoM9eebCBMzgFD0gVHdKt7Po").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new HttpRequestException("Greska prilikom rada sa API-em");
+                }
+                var content = response.Content.ReadAsStringAsync().Result;
+                var jsonObject = JsonConvert.DeserializeObject<JObject>(content);
+
+                List<AlbumInfo> albums = new List<AlbumInfo>();
+                foreach (var item in jsonObject["albums"]["items"])
+                {
+                    AlbumInfo w = new AlbumInfo();
+                    w.AlbumName = (string)item["name"];
+                    w.ArtistName = (string)item["artists"][0]["name"];
+                    w.ReleaseDate = (string)item["release_date"];
+                    w.TotalTracks = (int)item["total_tracks"];
+
+
+
+                    albums.Add(w);
+                }
+                return albums;
+            }
+            catch (Exception e)
+            {
+                throw new HttpRequestException($"Greska prilikom izvrsenja: {e.Message}");
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+    }
+}
---
+++ SysProgSpotifyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using SysProgSpotifyApp;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+var server = new HTTPServer();
+server.Start();
+Console.WriteLine("Press Enter to stop the server...");
+while (Console.ReadKey().Key != ConsoleKey.Enter)
+    server.Stop();
+Console.WriteLine("Server stopped!");
